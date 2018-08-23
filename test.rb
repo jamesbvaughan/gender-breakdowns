391d957f8e2d9882b2c85aa0a0d77d5e6974b7a7
James Vaughan <james@jamesbvaughan.com>
2018-08-23 16:42:07 -0700

    add test.rb

---
+++ test.rb
@@ -0,0 +1,45 @@
+require 'dotenv/load'
+require 'csv'
+require 'nokogiri'
+require 'http'
+require 'json'
+
+diary_file = 'watched.csv'
+tmdb_api_key = ENV['TMDB_API_KEY']
+
+men = 0
+women = 0
+unknown = 0
+count = 0
+
+CSV.foreach(diary_file, headers: true) do |movie|
+  unless movie.header_row?
+    movie_url = movie[3]
+
+    movie_doc = Nokogiri::HTML(HTTP.get(movie_url).to_s)
+
+    movie_tmdb_id = movie_doc.at_xpath('/html/body/@data-tmdb-id')
+
+    tmdb_url = "https://api.themoviedb.org/3/movie/#{movie_tmdb_id}/credits"
+
+    movie_credits = JSON.parse HTTP.get("#{tmdb_url}?api_key=#{tmdb_api_key}")
+
+    movie_credits['crew'].each do |credit|
+      next unless credit['job'] == 'Director'
+
+      case credit['gender']
+      when 0
+        puts "#{credit['name']} (#{credit['id']}): unknown"
+        unknown += 1
+      when 1
+        women += 1
+      when 2
+        men += 1
+      end
+
+      count += 1
+
+      puts "#{women} women, #{men} men, #{unknown} unknown, #{count} total"
+    end
+  end
+end
